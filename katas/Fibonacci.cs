2821745d7a9e6717ba1ccaec06bd97b50531d652
Petr Koula <koula.petr@gmail.com>
2023-11-21 13:26:03 +0100

    fibonacci recusrsive

--- katas/Fibonacci.cs
+++ katas/Fibonacci.cs
@@ -2,17 +2,17 @@
 
 public class Fibonacci
 {
-	public static int[] GenerateFibonacciList()
-	{
-		var length = 10;
-		var sequence = new List<int>() {1, 1};
+    public static int[] GenerateFibonacciList()
+    {
+        var length = 10;
+        var sequence = new List<int>() { 1, 1 };
 
-		for(var i = 2; i< length;i++)
-		{
-			sequence.Add(sequence[i - 1] + sequence[i - 2]);
-		}
+        for (var i = 2; i < length; i++)
+        {
+            sequence.Add(sequence[i - 1] + sequence[i - 2]);
+        }
 
-		return sequence.ToArray();
+        return sequence.ToArray();
     }
 
     public static int[] GenerateFibonacci()
@@ -28,6 +28,24 @@ public class Fibonacci
         }
 
         return sequence.ToArray();
+    }
+
+    public static IEnumerable<int> GenerateFibonacciRecursive()
+    {
+        static int fib(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            return fib(n - 1) + fib(n - 2);
+        }
+
+        var length = 10;
+
+        for (var i = 1; i < length; i++)
+        {
+            yield return fib(i);
+        }
     }
 }
 
