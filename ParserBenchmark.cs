7651aae3ed574fc29a24a889d8bb8ceb6483b359
Petr Koula <koula.petr@gmail.com>
2023-11-21 10:53:22 +0100

    first benchmark setup

---
+++ ParserBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class ParserBenchmark {
+
+    const string date = "20023-02-12";
+    private static readonly DateParser parser = new();
+
+    [Benchmark]
+    public void GetYearFromDate(DateOnly date)
+    {
+        parser.GetYearFromDate(date);
+    }
+
+    [Benchmark]
+    public void GetYearFromDate2(DateOnly date)
+    {
+        
+    }
+}
+
+public class DateParser
+{
+    public DateParser()
+    {
+    }
+
+    public void GetYearFromDate(DateOnly date)
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using Benchmarks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Benchmarkl start");
+
+        BenchmarkRunner.Run<ParserBenchmark>();
+    }
+}
