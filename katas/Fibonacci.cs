ccf127ba2f2351e418c74c3103ca5685b9319d64
Petr Koula <koula.petr@gmail.com>
2023-11-21 12:17:27 +0100

    added fibonacci with array

--- katas/Fibonacci.cs
+++ katas/Fibonacci.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace benchmarks.katas;
+﻿namespace benchmarks.katas;
 
 public class Fibonacci
 {
-	public static int[] GenerateFibonacci()
+	public static int[] GenerateFibonacciList()
 	{
 		var length = 10;
 		var sequence = new List<int>() {1, 1};
@@ -16,6 +13,21 @@ public class Fibonacci
 		}
 
 		return sequence.ToArray();
+    }
+
+    public static int[] GenerateFibonacci()
+    {
+        var length = 10;
+        var sequence = new int[length];
+        sequence[0] = 1;
+        sequence[1] = 1;
+
+        for (var i = 2; i < length; i++)
+        {
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
+        }
+
+        return sequence.ToArray();
     }
 }
 
