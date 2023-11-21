7d1f630423cdea985b719dda44d5f0d183c4b560
Petr Koula <koula.petr@gmail.com>
2023-11-21 12:08:05 +0100

    added fibonacci sequence

---
+++ katas/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace benchmarks.katas;
+
+public class Fibonacci
+{
+	public static int[] GenerateFibonacci()
+	{
+		var length = 10;
+		var sequence = new List<int>() {1, 1};
+
+		for(var i = 2; i< length;i++)
+		{
+			sequence.Add(sequence[i - 1] + sequence[i - 2]);
+		}
+
+		return sequence.ToArray();
+    }
+}
+
